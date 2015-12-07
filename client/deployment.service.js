5417e05426b33971b6a83658d6b4972e874b826e
febbweiss <fabrice.ecaille@gmail.com>
2015-12-07 15:56:38 +0000

    Feature: limit up to 10 deployment logs

--- client/deployment.service.js
+++ client/deployment.service.js
@@ -2,10 +2,10 @@ Deployments = new Mongo.Collection('deployments');
 
 DeploymentService = {
     list: function(id) {
-      var deployments = Deployments.find(),
+      var deployments = Deployments.find({}, {sort: {timestamp: -1}}),
             altered = deployments.map(function(doc, index, cursor) {
                 return _.extend(doc, {index: deployments.count() - index});
               });
-      return altered;  
+      return altered.slice(0, 10);  
     }
 };
